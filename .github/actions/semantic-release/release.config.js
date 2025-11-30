8ee877c87e8185256869a69a64e16a2cbc6d589b
Lucas Rios <lucasrius@gmail.com>
2025-11-30 19:59:46 -0300

    Lint Node

--- .github/actions/semantic-release/release.config.js
+++ .github/actions/semantic-release/release.config.js
@@ -1,17 +1,16 @@
 const config = {
-    branches: ['main','master'],
-    plugins: [
-       
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-     ["@semantic-release/git", {
-      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+  branches: ['main', 'master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    ['@semantic-release/git', {
+      assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
+      // CORREÇÃO AQUI: Use backticks (`) em vez de aspas simples (')
+      message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
     }],
-    "@semantic-release/github"
-  ]
-    
+    '@semantic-release/github',
+  ],
 };
 
 module.exports = config;
